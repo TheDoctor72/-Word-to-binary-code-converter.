395f4a87504c25f882aba6e32e23940e4d48f83e
TheDoctor72 <ziotogni@gmail.com>
2024-10-19 00:47:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 def text_to_binary(text):
     binary_result=''.join(format(ord(char),'08b')for char in text)
     return binary_result
-text="Hello World"
+print("Write your text here!")
+text=input()
 binary= text_to_binary(text)
 print(f'Original text: {text}')
 print(f'Binary code:{binary}')
